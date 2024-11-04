4e6f678f7f347e99ca58a1f0a0e97410e7280f55
kabikira <kabikirar2@protonmail.com>
2024-11-04 22:20:31 +0900

    svg画像

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,9 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:intl/intl.dart';
+import 'gen/assets.gen.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      /*
-      localizationsDelegates: [
-        GlobalWidgetsLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-       */
-      localizationsDelegates: L10n.localizationsDelegates,
-      /*
-      supportedLocales: [
-        Locale('ja', 'JP'),
-      ],
-       */
-      supportedLocales: L10n.supportedLocales,
       home: HomeScreen(),
     )
   );
@@ -30,24 +14,13 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // デフォルトロケールの設定
-    Intl.defaultLocale = Localizations.localeOf(context).toString();
-    final l10n = L10n.of(context);
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const TextField(),
-            Text(
-              DateFormat.yMEd().format(DateTime.now()),
-            ),
-            Text(l10n.helloWorld),
-            Text(l10n.numOfSearchResult(0)),
-            Text(l10n.numOfSearchResult(1)),
-            Text(l10n.numOfSearchResult(2)),
-          ],
-        ),
+        child:
+          Assets.check.svg(
+            width: 72,
+            height: 72,
+          ),
       ),
     );
   }
